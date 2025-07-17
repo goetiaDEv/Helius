21d56f06dedad0ac9849ab93d59737a3ecfa6371
Ezequiel Abreu <goetia.dev@proton.me>
2025-07-17 12:24:43 -0300

    Add files via upload

---
+++ ScanResult.cs
@@ -0,0 +1,16 @@
+
+namespace Helius.Models
+{
+    public class ScanResult
+    {
+        // Esta classe pode ser expandida para armazenar resultados detalhados do scan.
+        // Por exemplo:
+        // public string IpAddress { get; set; }
+        // public int Port { get; set; }
+        // public string ServiceName { get; set; }
+        // public string Banner { get; set; }
+        // public List<string> CvesFound { get; set; }
+    }
+}
+
+
